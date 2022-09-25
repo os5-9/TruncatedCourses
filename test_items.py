925bdc71d1c37a5bd7561e531d16c887f24c4c23
os5-9 <75325366+os5-9@users.noreply.github.com>
2022-09-25 14:06:35 +0400

    remove 'ru' from link

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_find_button(driver):
